87333d3cf6905955b5b76c44793a0fd24e29285a
mequam <blue9ja@gmail.com>
2025-01-15 15:01:32 -0500

    added a second public subnet for the load balancer

--- cloudformation/create_stack.sh
+++ cloudformation/create_stack.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 aws cloudformation create-stack \
     --stack-name test-bucket-stack \
-    --template-body file://$(pwd)/bucket.yml \
+    --template-body file://$(pwd)/project-2-stack.yml \
     --parameters ParameterKey=VPCCidr,ParameterValue=192.168.0.0/16 \
                  ParameterKey=PublicSubnetCidr,ParameterValue=192.168.1.0/24 \
+                 ParameterKey=PublicSubnetCidr2,ParameterValue=192.168.4.0/24 \
                  ParameterKey=PrivateSubnetCidr,ParameterValue=192.168.2.0/24 \
                  ParameterKey=PrivateDBSubnetCidr,ParameterValue=192.168.3.0/24 \
                  ParameterKey=AvailabilityZone1,ParameterValue=us-east-1a \
